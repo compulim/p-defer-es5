1f12f9c5305ea17b34535f08d9d105ae803b33be
William Wong <compulim@users.noreply.github.com>
2022-02-28 14:27:13 +0000

    Clean up folder hierarchy

--- __e2e__/runNode.js
+++
@@ -1,7 +0,0 @@
-const createDeferred = require('p-defer-es5');
-
-const { promise, resolve } = createDeferred();
-
-setTimeout(() => resolve(123), 0);
-
-promise.then(process.send.bind(process));
--- __e2e__/runNode.mjs
+++
@@ -1,7 +0,0 @@
-import createDeferred from 'p-defer-es5';
-
-const { promise, resolve } = createDeferred();
-
-setTimeout(() => resolve(123), 0);
-
-promise.then(process.send.bind(process));
